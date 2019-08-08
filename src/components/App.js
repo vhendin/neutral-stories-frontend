a4d4b520403acdf3d5a0d224caed43b49e831ccd
Magnus Vhendin <magnus.vhendin@gmail.com>
2019-08-08 14:29:06 +0200

    working routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from '../logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Route path="/" component={Home} />
       </header>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+
+function Home() {
+  return (
+    <div className="Home">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default Home;
